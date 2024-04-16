d63591316c2b17743b098b1056e2a84af2d73e62
mondragon-ai <angel.mondragon@yahoo.com>
2024-04-16 17:29:33 -0300

    Update to live server

--- src/third-party-apis/server.ts
+++ src/third-party-apis/server.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosRequestConfig} from "axios";
-const URL = false
+const URL = true
   ? "https://us-central1-devve-cod.cloudfunctions.net"
   : "http://127.0.0.1:5001/devve-cod/us-central1";
 
